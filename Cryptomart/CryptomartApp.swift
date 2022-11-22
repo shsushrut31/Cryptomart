7b1c60cf37e6b10bf1d2d37a318c24fd44e46392
Sushrut Shastri <shsushrut31@gmail.com>
2022-11-22 11:36:52 -0700

    Initial Commit

---
+++ Cryptomart/CryptomartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptomartApp.swift
+//  Cryptomart
+//
+//  Created by Sushrut Shastri on 2022-11-22.
+//
+
+import SwiftUI
+
+@main
+struct CryptomartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
